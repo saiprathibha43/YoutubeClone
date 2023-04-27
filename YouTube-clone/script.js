cbbefbd6749c53ee19b6f931ff2a07207b43ecf8
saiprathibha43 <115211892+saiprathibha43@users.noreply.github.com>
2023-04-27 16:39:55 +0530

    Add files via upload

---
+++ YouTube-clone/script.js
@@ -0,0 +1,27 @@
+var menuIcon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+menuIcon.onclick = function() {
+  sidebar.classList.toggle("small-sidebar");
+  container.classList.toggle("large-container");
+}
+
+//moving scrollbar with a click
+
+
+
+
+const tabList = document.querySelector(".scrollable-tabs-container ul");
+const rightArrow = document.querySelector(".scrollable-tabs-container .right-arrow svg");
+const leftArrow = document.querySelector(".scrollable-tabs-container .left-arrow svg");
+rightArrow.addEventListener("click", () => {
+  tabList.scrollLeft += 200;
+});
+leftArrow.addEventListener("click", () => {
+  tabList.scroll -= 200;
+});
+
+
+var cont = document.getElementById("bdy");
+cont.style.height = (window.innerHeight);
+cont.style.width = window.innerWidth;
